c565da2fe3c636d453ab5e5c4b1180900d4df8d9
Carlos Gomes <kmartinho8@gmail.com>
2017-08-29 16:31:55 +0200

    rm build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gocarlos/git/unit_tests_example
-# Build directory: /home/gocarlos/git/unit_tests_example/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "unit_test_example_test")
-subdirs("external/gtest")
--- build/external/gtest/src/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gocarlos/git/unit_tests_example/build/external/gtest/src/googletest/googletest
-# Build directory: /home/gocarlos/git/unit_tests_example/build/external/gtest/src/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
