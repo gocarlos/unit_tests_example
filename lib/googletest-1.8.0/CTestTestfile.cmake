08706a4215e72f61cf45ebe3f52446223fcf8416
Carlos Gomes <kmartinho8@gmail.com>
2017-08-29 16:32:41 +0200

    rm gtest library, as it is now an external project

--- lib/googletest-1.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gocarlos/git/interview_rapyuta/lib/googletest-1.8.0
-# Build directory: /home/gocarlos/git/interview_rapyuta/lib/googletest-1.8.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
--- lib/googletest-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gocarlos/git/interview_rapyuta/lib/googletest-1.8.0/googletest
-# Build directory: /home/gocarlos/git/interview_rapyuta/lib/googletest-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
